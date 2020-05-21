8b54ccd9b056d0e6027cb7e2bac09650ba0ceb81
ArkovKonstantin <arkov.constantin@yandex.ru>
2020-05-21 06:07:33 +0300

    v2

--- hw_7_parse_logs/main.py
+++ hw_7_parse_logs/main.py
@@ -20,7 +20,7 @@ def create_df_row(w: list, user_id: str):
         row.append(el[1])
         row.append(el[0])
     row.append(user_id)
-    # print(w, len(w))
+    print(w, len(w))
     # if len(row) == 1:
     #     print(row)
     return row
@@ -62,10 +62,9 @@ def prepare_train_set(logs_path: str, session_length: int, window_size: int, max
 
                     d2 = get_time(fast[0])
                     # check if window is reached
-                    d3 = d2 - d1
-                    if d3.total_seconds() / 60 > max_duration or len(window) == session_length:
+                    if len(window) == session_length and (d2 - d1).total_seconds() / 60 <= max_duration:
                         # sessions.append(window)
-                        print(window, len(window))
+                        # print(window, len(window))
                         yield create_df_row(window, user_id)
 
                         if window_size < len(window):
@@ -73,12 +72,13 @@ def prepare_train_set(logs_path: str, session_length: int, window_size: int, max
                             slow = window[0]
                             slow_idx += window_size
                             d1 = get_time(slow[0])
+                            window.append(fast)
                         elif window_size == len(window):
                             slow = fast
                             slow_idx = curr_idx
                             d1 = get_time(slow[0])
-                            print("slow", slow_idx)
-                            window = []
+                            window = [fast]
+
                         elif window_size > len(window):
                             slow = fast
                             slow_idx = curr_idx
@@ -86,16 +86,25 @@ def prepare_train_set(logs_path: str, session_length: int, window_size: int, max
                             window = []
                             continue
 
-                    # processing edge case
-                    if (d2 - d1).total_seconds() / 60 > max_duration:
-                        # sessions.append(window)
-                        yield create_df_row(window, user_id)
+                    elif len(window) != session_length and (d2 - d1).total_seconds() / 60 > max_duration:
+                        if window:
+                            yield create_df_row(window, user_id)
+
                         slow = fast
                         slow_idx = curr_idx
                         d1 = get_time(slow[0])
-                        window = []
+                        window = [slow]
 
-                    window.append(fast)
+                    elif len(window) == session_length and (d2 - d1).total_seconds() / 60 > max_duration:
+                        yield create_df_row(window, user_id)
+
+                        slow = fast
+                        slow_idx = curr_idx
+                        d1 = get_time(slow[0])
+                        window = [slow]
+
+                    elif len(window) != session_length and (d2 - d1).total_seconds() / 60 <= max_duration:
+                        window.append(fast)
 
                 # sessions.append(window)
                 yield create_df_row(window, user_id)
@@ -111,10 +120,10 @@ def prepare_train_set(logs_path: str, session_length: int, window_size: int, max
 
 if __name__ == '__main__':
     start = datetime.datetime.now()
-    df = prepare_train_set(LOGS_PATH, 10, 10, 30)
-    print(df)
-    # df = prepare_train_set('./user_logs_example.csv', 4, 2, 30)
+    # df = prepare_train_set(LOGS_PATH, 10, 10, 30)
     # print(df)
+    df = prepare_train_set('./user_logs_example.csv', 4, 2, 30)
+    print(df)
     print((datetime.datetime.now() - start).seconds, "s")
 
     # import pstats
